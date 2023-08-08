88c4448b77e4269d43a7c326487c941516ea3d24
hojipkim <rlaghwlq12@gmail.com>
2023-08-08 17:51:49 +0900

    Branch move

--- app.py
+++ app.py
@@ -14,17 +14,17 @@ collection3 = db['C']
 def loginRender():
     return render_template("login.html")
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def registerRender():
     return render_template("register.html")
 
-@app.route('/main')
+@app.route('/main', methods=['POST'])
 def mainRender():
     return render_template("main.html")
 
-@app.route('/login')
+@app.route('/login' , methods=['POST'])
 def login():
-    return 
+    return 'hello'
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=4000, debug=True)
