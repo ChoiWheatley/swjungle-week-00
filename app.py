db6d733bda19e7956b02f292a7da0db5a5540715
hojipkim <rlaghwlq12@gmail.com>
2023-08-08 15:45:56 +0900

    Committed

--- app.py
+++ app.py
@@ -24,7 +24,8 @@ def mainRender():
 
 @app.route('/login')
 def login():
-    return 
+    return render_template("main.html")
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=4000, debug=True)
+    
